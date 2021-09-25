648ae451e23007415d6a08999742d6a6013d1ecc
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-24 18:42:15 -0600

    user controller built

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+
+  before_action :find_user, except: [ :create ]
+
+  def create
+    user = User.create(user_params)
+    render json: user, status: :created
+  end
+
+  def show
+    render json: @user, stauts: :ok
+  end
+
+  def update
+    @user.update(user_params)
+    render json: @user, status: :accepted
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_name, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find_by!(id: session[:user_id])
+  end
 end
