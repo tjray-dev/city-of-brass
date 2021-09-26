ef6308bde416c1e548232cdec07ff268de800938
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-25 18:41:36 -0600

    create action

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,12 @@
 class CharactersController < ApplicationController
+
+  def create
+    character = Character.create(character_params)
+  end
+
+  private 
+
+  def character_params
+    params.permit(:character_name, :character_origin)
+  end
 end
