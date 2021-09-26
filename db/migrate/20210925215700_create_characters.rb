9e4a51fa94d2a928d7aece73fa0c532dda3f7247
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-25 18:30:25 -0600

    validations complete need to look into enum

--- db/migrate/20210925215700_create_characters.rb
+++ db/migrate/20210925215700_create_characters.rb
@@ -2,11 +2,11 @@ class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     create_table :characters do |t|
       t.string :character_name
-      t.string :character_origin
-      t.integer :character_type
-      t.integer :character_hp
-      t.integer :attack_bonus
-      t.integer :defense_bonus
+      t.text :character_origin
+      t.integer :character_type, default: 0
+      t.integer :character_hp, default: 100
+      t.integer :attack_bonus, default: 1
+      t.integer :defense_bonus, default: 1
 
       t.timestamps
     end
