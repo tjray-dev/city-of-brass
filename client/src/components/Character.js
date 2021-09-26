6b04f6a4e5efac95e2fde8afb10d5b99bb7c12c9
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-26 14:49:34 -0600

    basic character creation form built

--- client/src/components/Character.js
+++ client/src/components/Character.js
@@ -1,6 +1,20 @@
 const Character = () => {
   return (
     <>
+      <div>
+        <form>
+          <input type="text"
+            name="character_name"
+            placeholder="Character Name"
+          />
+          <select name="cars" id="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+        </form>
+      </div>
     </>
   )
 }
