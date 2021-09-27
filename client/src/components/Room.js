b3321cf9934bd2ecaf0649f69f383a9fb3b4a708
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-27 13:14:03 -0600

    installed redux/react-redux

---
+++ client/src/components/Room.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const Room = ({ character }) => {
+
+  // States
+    // Character
+    // Monster
+    // Skills
+    // Items
+    // Room
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Room
