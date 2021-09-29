46c02b17b1dc5c9a3da6f8ef88633ff4f98f4555
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-28 22:04:17 -0600

    actions added migration altered

--- app/controllers/character_skills_controller.rb
+++ app/controllers/character_skills_controller.rb
@@ -1,2 +1,18 @@
 class CharacterSkillsController < ApplicationController
+
+  def create
+    character_skill = CharacterSkill.create!(character_skill_params)
+    render json: character_skill, status: :created
+  end
+
+  def show
+    character_skill = CharacterSkill.find(params[:id])
+    render json: character_skill, status: :ok
+  end
+
+  def update
+    character_skill = CharacterSkill.find(params[:id])
+    character_skill.update!(params[:level])
+    render json: character_skill, status: :accepted
+  end
 end
--- db/migrate/20210929033901_create_character_skills.rb
+++ db/migrate/20210929033901_create_character_skills.rb
@@ -3,6 +3,7 @@ class CreateCharacterSkills < ActiveRecord::Migration[6.1]
     create_table :character_skills do |t|
       t.belongs_to :skill, null: false, foreign_key: true
       t.belongs_to :character, null: false, foreign_key: true
+      t.integer :level, default: 0
 
       t.timestamps
     end
