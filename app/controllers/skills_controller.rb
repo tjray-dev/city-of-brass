7525388935b2328b8a64faefa31615fc1b84d9ff
tjray-dev <64376864+tjray-dev@users.noreply.github.com>
2021-09-28 21:04:51 -0600

    skills actions and routes added

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,7 @@
 class SkillsController < ApplicationController
+
+  def show
+    skill = Skill.find_by!(name: params[:name])
+    render json: skill, status: :ok
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,5 @@
 class Character < ApplicationRecord
 
-
-
   validates :character_name, :character_origin, presence: true
   validates :character_name, length: { in: 2..20 }
   validates :attack_bonus, :defense_bonus, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
